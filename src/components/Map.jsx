a7d5fe32997b2753a5e253fafaf155cb0a363e8e
shraddha dongol <shraddhadongol59@gmail.com>
2025-10-09 22:03:02 +0545

    restro detail page inprogres

---
+++ src/components/Map.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 27.7172, // Kathmandu
+  lng: 85.3240,
+};
+
+const GoogleMapComponent = () => {
+  return (
+    // <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    //   <GoogleMap
+    //     mapContainerStyle={containerStyle}
+    //     center={center}
+    //     zoom={13}
+    //   >
+    //     <Marker position={center} />
+    //   </GoogleMap>
+    // </LoadScript>
+
+    <>
+    Map
+    </>
+  );
+};
+
+export default GoogleMapComponent;
--- src/pages/User/Restaurants/RestaurantDetail.jsx
+++ src/pages/User/Restaurants/RestaurantDetail.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import RestaurantCard from "../../../components/RestaurantCard";
+import GoogleMapComponent from "../../../components/Map";
 
 const RestaurantDetail = ({id}) => {
     return(
         <div id={id}>
-WIndowpane.cafe
+            <div className="image-group">
 
+            </div>
+
+            <div className="restro-details">
+                <div className="restro-title">
+                    Cafe Windowpane
+                </div>
+                <div className="restro-desc">
+                    Cafe Windowpane is a charming cafe located in the heart of the city, offering a cozy atmosphere and a delightful menu of coffee and pastries.
+                </div>
+                <div className="restro-metadata">
+                    Lalitpur 44600, Nepal
+                    7:00 AM - 10:00 PM
+                </div>
+
+                <div className="restro-categories">
+                    breakfast, lunch, dinner
+                </div>
+
+                <div className="restro-menu">
+
+                </div>
+
+                <div className="restro-location-map">
+                   <GoogleMapComponent />
+                </div>
+
+                <div className="restro-review">
+                    Reviews
+                </div>
+
+                <div className="restro-faqs">
+                    FAQs
+                </div>
+            </div>
+
+            <div className="restro-recommendations grid grid-cols-12 gap-4">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+
+            </div>
 
         </div>
         
